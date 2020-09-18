355856218cec917cebec264e4cf0c37dc7a8e658
Santiago Leiva <santiagoeleiva@gmail.com>
2020-09-18 14:09:25 -0300

    feat(custom_emojis): implementa select and copy

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,4 @@
-var app = new Vue({
+new Vue({
     el: "#banner-app",
     data: {
         title: "Slack banner generator",
@@ -10,12 +10,21 @@ var app = new Vue({
             var newOutput = "";
 
             if (this.input) {
-                Figlet.write(this.input, "default-font", function (output) {
-                    newOutput = " " + output;
+                var input = " " + this.input;
+                Figlet.write(input, "default-font", function (output) {
+                    newOutput += output;
                 })
             }
 
             this.output = newOutput;
+        },
+        copyOutput: function () {
+            document.querySelector("#output").select();
+            try {
+                document.execCommand('copy');
+            } catch (err) {
+                console.error("Error copying selection");
+            }
         }
     }
 });
