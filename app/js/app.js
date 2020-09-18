9d97cdd7f31fbc64af81dca256d80f08d16f522e
Santiago Leiva <santiagoeleiva@gmail.com>
2020-09-18 15:59:23 -0300

    feat(custom_emojis): WIP - agrega custom emojis

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,5 @@
-var defaultBGChar = ".";
-var defaultTextChar = "#";
+var bgChar = ".";
+var textChar = "#";
 
 var defaultBGEmoji = ":white_square:";
 var defaultTextEmoji = ":black_square:";
@@ -10,18 +10,23 @@ new Vue({
         title: "Slack banner generator",
         input: "",
         output: "",
-        displayCopiedMessage: false
+        displayCopiedMessage: false,
+        bgEmoji: defaultBGEmoji,
+        textEmoji: defaultTextEmoji
     },
     methods: {
         handleInputChange: function () {
             var newOutput = "";
 
             if (this.input) {
+                let bgEmoji = this.bgEmoji;
+                let textEmoji = this.textEmoji;
                 var input = " " + this.input;
+
                 Figlet.write(input, "hash-default-font", function (output) {
                     newOutput += output;
-                    newOutput = newOutput.replaceAll(defaultBGChar, defaultBGEmoji);
-                    newOutput = newOutput.replaceAll(defaultTextChar, defaultTextEmoji);
+                    newOutput = newOutput.replaceAll(bgChar, bgEmoji);
+                    newOutput = newOutput.replaceAll(textChar, textEmoji);
                 })
             }
 
@@ -38,6 +43,34 @@ new Vue({
         },
         closeCopiedMessage: function () {
             this.displayCopiedMessage = false;
+        },
+        onBGEmojiChange: function () {
+            if (this.bgEmoji) {
+                var bgEmoji = this.bgEmoji;
+                var textEmoji = this.textEmoji;
+                var newOutput = "";
+                var input = " " + this.input;
+                Figlet.write(input, "hash-default-font", function (output) {
+                    newOutput += output;
+                    newOutput = newOutput.replaceAll(bgChar, bgEmoji);
+                    newOutput = newOutput.replaceAll(textChar, textEmoji);
+                });
+                this.output = newOutput;
+            }
+        },
+        onTextEmojiChange: function () {
+            if (this.textEmoji) {
+                var bgEmoji = this.bgEmoji;
+                var textEmoji = this.textEmoji;
+                var newOutput = "";
+                var input = " " + this.input;
+                Figlet.write(input, "hash-default-font", function (output) {
+                    newOutput += output;
+                    newOutput = newOutput.replaceAll(bgChar, bgEmoji);
+                    newOutput = newOutput.replaceAll(textChar, textEmoji);
+                });
+                this.output = newOutput;
+            }
         }
     }
 });
